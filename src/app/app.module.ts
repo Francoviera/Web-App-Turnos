aab82042c3acd328b3d56e8809894ad789f7e017
Francoviera <vierafranco1@gmail.com>
2020-10-23 20:23:57 -0300

    se agrego el poder mostrar eventos en el calendario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import dayGridPlugin from '@fullcalendar/daygrid';
+// import dayGridPlugin from '@fullcalendar/daygrid';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TurnosComponent } from './turnos/turnos.component';
 
-FullCalendarModule.registerPlugins([ // register FullCalendar plugins
-  dayGridPlugin
-]);
+// FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+//   dayGridPlugin
+// ]);
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/turnos/turnos.component.ts
+++ src/app/turnos/turnos.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
+import { CalendarContent, CalendarData, CalendarDataProvider, CalendarOptions } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 
 @Component({
   selector: 'app-turnos',
@@ -8,11 +12,69 @@ import { CalendarOptions } from '@fullcalendar/angular';
 })
 export class TurnosComponent implements OnInit {
 
-  constructor() { }
+  eventsDB: any; //Averiguar de que tipo es esta variable
+  calendarOptions: CalendarOptions; 
+  lang: string;
+
+
+  constructor() { 
+    this.lang= navigator.language;
+    this.eventsDB = [
+      {
+        title: "El Pepaaa",
+        start: '2020-10-23T10:00:00',
+        end: '2020-10-23T16:00:00',
+        description: "ndeah"
+      },
+      {
+        title: "El Pepe",
+        start: '2020-11-10T10:00:00',
+        end: '2020-11-10T16:00:00',
+      },
+      {
+        title: "El Pepa",
+        start: '2020-10-10T10:00:00',
+        end: '2020-10-10T16:00:00',
+      },
+      {
+        title: "El Pepa",
+        start: '2020-10-10T16:30:00',
+        end: '2020-10-10T17:00:00',
+        description:"sape",
+      }
+    ] 
+    this.calendarOptions= {
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      // initialView: 'dayGridMonth', //Ver para que sirve
+      events: this.eventsDB,
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      // themeSystem: 'Bostrap', // Investigar como cambiar el tema del Calendario
+      timeZone: 'UTC',
+      locale: this.lang,
+      dateClick: (e) =>  {
+        console.log(e); //Esto serviria para ir a otro Componente con detalles sobre el evento
+      },      
+    }
+  }
+
+  //Ver como implementar el Hover para ver la description del evento
+  viewDetail(item){
+    console.log(item);
+  }
+  // viewDetail(info) {
+  //   var tooltip = new Tooltip(info.el, {
+  //     title: info.event.extendedProps.description,
+  //     placement: 'top',
+  //     trigger: 'hover',
+  //     container: 'body'
+  //   });
+  // }
 
   ngOnInit(): void {
-  }
-  calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth'
-  };
+    console.log(this.lang);
+  } 
 }
